4920ea060ab08c5cf7bc46c78e7cbf91897ded1b
jkramr <yuliya.kramarenko@gmail.com>
2017-03-12 15:31:50 +0200

    Reformat code. Change gradle version to a non-buggy one.

--- src/main/java/com/jkramr/WebCrawlerApplication.java
+++ src/main/java/com/jkramr/WebCrawlerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebCrawlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebCrawlerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WebCrawlerApplication.class, args);
+  }
 }
